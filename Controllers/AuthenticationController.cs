ec06dc723338e1fd8a11fe06345a99df7e608750
Danila Samusev <danila.samusev@mail.ru>
2019-03-10 16:19:34 +0300

    Login page minimal logic has been implemented

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,13 +1,24 @@
+using AccountingSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountingSystem.Controllers
 {
     public class AuthenticationController : Controller
     {
-        
+        [HttpGet]
         public IActionResult Login()
         {
-            return View();           
+            LoginModel defaultModel = new LoginModel();
+            defaultModel.LoginError = "";
+            defaultModel.PasswordError = "";
+            
+            return View(defaultModel);           
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {                                  
+            return View(model);
         }
     }
 }
